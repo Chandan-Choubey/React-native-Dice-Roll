cde44cd5e317a4f636c125ba54fc8cca314a69a5
DEVP-CHANDAN <choubeychandan2463@gmail.com>
2024-10-01 21:12:44 +0530

    Dice Roll

--- App.tsx
+++ App.tsx
@@ -1,118 +1,70 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import {Image, Pressable, StyleSheet, Text, View} from 'react-native';
+import ReactNativeHapticFeedback from 'react-native-haptic-feedback';
+import React, {useState} from 'react';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
+  const [image, setImage] = useState(
+    'https://static-00.iconduck.com/assets.00/dice-1-icon-512x512-fr5yy7n7.png',
+  );
+  const options = {
+    enableVibrateFallback: true,
+    ignoreAndroidSystemSettings: false,
+  };
+  const images = [
+    'https://static-00.iconduck.com/assets.00/dice-1-icon-512x512-fr5yy7n7.png',
+    'https://static-00.iconduck.com/assets.00/dice-2-icon-512x512-qqy2ux1a.png',
+    'https://static-00.iconduck.com/assets.00/dice-3-icon-512x512-lepd9rnr.png',
+    'https://static-00.iconduck.com/assets.00/dice-4-icon-512x512-1o0k2kqo.png',
+    'https://static-00.iconduck.com/assets.00/dice-5-icon-512x512-0ps13pr4.png',
+    'https://static-00.iconduck.com/assets.00/dice-6-icon-512x512-9n12hr9i.png',
+  ];
+  const generateRandomImage = () => {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    setImage(images[randomIndex]);
+    ReactNativeHapticFeedback.trigger('impactLight', options);
+  };
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <View style={styles.imageView}>
+        <Image
+          style={styles.image}
+          source={{
+            uri: image,
+          }}
+        />
+      </View>
+      <Pressable onPress={generateRandomImage} style={styles.btnContainer}>
+        <Text style={styles.btn}>Roll Dice</Text>
+      </Pressable>
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    backgroundColor: '#ecd9dd',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  imageView: {
+    marginBottom: 20,
   },
-  sectionDescription: {
-    marginTop: 8,
+  image: {
+    height: 100,
+    width: 100,
+  },
+  btnContainer: {
+    width: '100%',
+    alignItems: 'center',
+  },
+  btn: {
+    backgroundColor: '#008080',
+    padding: 10,
+    borderRadius: 5,
+    color: '#fff',
+    fontWeight: 'bold',
     fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+    textAlign: 'center',
   },
 });
-
-export default App;
